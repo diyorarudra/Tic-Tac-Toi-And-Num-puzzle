d6b4e9bb1007add7c45cba9900d46d6716b15448
Rudra Diyora <rudradiyora63@gmail.com>
2023-09-11 17:40:30 +0530

    first commit update

---
+++ Assets/Scenes/numpuz.cs
@@ -0,0 +1,244 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class numpuz : MonoBehaviour
+{
+    public Text[] t;
+    public Text w;
+    public string[] s = { "1","2","3","4","5","6","7","","8"}; 
+    public Button[] b;
+    int cnt = 0;
+    // Start is called before the first frame update
+
+    public void OnChange()
+    {
+        SceneManager.LoadScene("SampleScene");
+        print("bgfda");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Start()
+    {
+        oct();
+    }
+     
+    
+    void oct()
+    {
+        for (int ii = 0; ii < s.Length; ii++)
+        {
+            int r = Random.Range(0, s.Length);
+            string temp = s[r];
+            s[r] = s[ii];
+            s[ii] = temp;
+           
+           
+        }
+        for (int j = 0; j < s.Length; j++)
+        {
+            t[j].text = s[j];
+
+        }
+    }
+    public void move(int i)
+    {
+        if (i == 0)
+        {
+
+            if (t[1].text == "")
+            {
+                t[1].text = t[0].text;
+                t[0].text = "";
+            }
+            else if (t[3].text == "")
+            {
+                t[3].text = t[0].text;
+                t[0].text = "";
+            }
+            win();
+        }
+        else if (i == 1)
+        {
+            if (t[0].text == "")
+            {
+                t[0].text = t[1].text;
+                t[1].text = "";
+            }
+            else if (t[4].text == "")
+            {
+                t[4].text = t[1].text;
+                t[1].text = "";
+            }
+            else if (t[2].text == "")
+            {
+                t[2].text = t[1].text;
+                t[1].text = "";
+            }
+            win();
+        }
+        else if (i == 2)
+        {
+            if (t[1].text == "")
+            {
+                t[1].text = t[2].text;
+                t[2].text = "";
+            }
+            else if (t[5].text == "")
+            {
+                t[5].text = t[2].text;
+                t[2].text = "";
+            }
+            win();
+        }
+        else if (i == 3)
+        {
+            if (t[0].text == "")
+            {
+                t[0].text = t[3].text;
+                t[3].text = "";
+            }
+            else if (t[4].text == "")
+            {
+                t[4].text = t[3].text;
+                t[3].text = "";
+            }
+            else if (t[6].text == "")
+            {
+                t[6].text = t[3].text;
+                t[3].text = "";
+            }
+            win();
+        }
+        else if (i == 4)
+        {
+            if (t[1].text == "")
+            {
+                t[1].text = t[4].text;
+                t[4].text = "";
+            }
+            else if (t[3].text == "")
+            {
+                t[3].text = t[4].text;
+                t[4].text = "";
+            }
+            else if (t[7].text == "")
+            {
+                t[7].text = t[4].text;
+                t[4].text = "";
+            }
+            else if (t[5].text == "")
+            {
+                t[5].text = t[4].text;
+                t[4].text = "";
+            }
+            win();
+        }
+        else if (i == 5)
+        {
+            if (t[2].text == "")
+            {
+                t[2].text = t[5].text;
+                t[5].text = "";
+            }
+            else if (t[4].text == "")
+            {
+                t[4].text = t[5].text;
+                t[5].text = "";
+            }
+            else if (t[8].text == "")
+            {
+                t[8].text = t[5].text;
+                t[5].text = "";
+            }
+            win();
+        }
+        else if (i == 6)
+        {
+            if (t[3].text == "")
+            {
+                t[3].text = t[6].text;
+                t[6].text = "";
+            }
+            else if (t[7].text == "")
+            {
+                t[7].text = t[6].text;
+                t[6].text = "";
+            }
+            win();
+        }
+        else if (i == 7)
+        {
+            if (t[6].text == "")
+            {
+                t[6].text = t[7].text;
+                t[7].text = "";
+            }
+            else if (t[4].text == "")
+            {
+                t[4].text = t[7].text;
+                t[7].text = "";
+            }
+            else if (t[8].text == "")
+            {
+                t[8].text = t[7].text;
+                t[7].text = "";
+            }
+            win();
+        }
+        else if (i == 8)
+        {
+            if (t[5].text == "")
+            {
+                t[5].text = t[8].text;
+                t[8].text = "";
+            }
+            else if (t[7].text == "")
+            {
+                t[7].text = t[8].text;
+                t[8].text = "";
+            }
+            win();
+        }
+        else if (i == 9)
+        {
+            w.text = "";
+            for (int ii = 0; ii < 9; ii++)
+            {
+                b[ii].interactable = true;
+                t[ii].text = "";
+                cnt = 0;
+
+            }
+            for (int ia = 0; ia < s.Length; ia++)
+            {
+                t[ia].text = s[ia];
+            }
+        }
+    }
+   
+    void win()
+    {
+        if (t[0].text == "1" && t[1].text == "2" && t[2].text == "3" && t[3].text == "4" && t[4].text == "5" && t[5].text == "6" && t[6].text == "7" && t[7].text == "8" && t[8].text == "")
+        {
+            hide();
+            w.text = "YOU WIN.."+w.ToString();
+
+        }
+    }
+
+            void hide()
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    b[i].interactable = false;
+                }
+            }
+        }
+
+
---
+++ Assets/Scenes/tictac.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class tictac : MonoBehaviour
+{
+   
+    public Text[] b;
+    public Button[] d;
+    public Text l,rudra,xx,oo;
+    int cnt = 0,xm=0,om=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void tic(int i)
+    {
+       
+            if (cnt % 2 == 0)
+            {
+                b[i].text = "X";
+                d[i].interactable = false;
+                cnt++;
+                rudra.text = "Move 0 "+rudra.ToString();
+                win();
+            }
+            else
+            {
+                b[i].text = "0";
+                d[i].interactable = false;
+                cnt++;
+            rudra.text = "Move X " + rudra.ToString();
+            win();
+            }
+        
+    }
+    public void res()
+    {
+        tru();  
+        clean();
+        l.text = "";
+        rudra.text = "";
+    }
+    void win()
+    {
+        if (b[0].text == "X" && b[1].text == "X" && b[2].text == "X" || b[3].text == "X" && b[4].text == "X" && b[5].text == "X" || b[6].text == "X" && b[7].text == "X" && b[8].text == "X" || b[0].text == "X" && b[3].text == "X" && b[6].text == "X" || b[1].text == "X" && b[4].text == "X" && b[7].text == "X" || b[2].text == "X" && b[5].text == "X" && b[8].text == "X" || b[0].text == "X" && b[4].text == "X" && b[8].text == "X" || b[2].text == "X" && b[4].text == "X" && b[6].text == "X")
+        {
+            l.text = "X is win..." + l.ToString();
+            fal();
+        
+            xm++;
+
+        }
+        else if (b[0].text == "0" && b[1].text == "0" && b[2].text == "0" || b[3].text == "0" && b[4].text == "0" && b[5].text == "0" || b[6].text == "0" && b[7].text == "0" && b[8].text == "0" || b[0].text == "0" && b[3].text == "0" && b[6].text == "0" || b[1].text == "0" && b[4].text == "0" && b[7].text == "0" || b[2].text == "0" && b[5].text == "0" && b[8].text == "0" || b[0].text == "0" && b[4].text == "0" && b[8].text == "0" || b[2].text == "0" && b[4].text == "0" && b[6].text == "0")
+        {
+            l.text = "0 is win..."+l.ToString();
+            fal();
+            om++;
+        }
+    }
+    void kingo()
+    {
+        if (cnt == 0)
+        {
+            b[0].text = "0";
+            cnt++;
+        }
+        else if (b[0].text == "X" && b[1].text == "X" && b[2].text == "X" || b[3].text == "X" && b[4].text == "X" && b[5].text == "X" || b[6].text == "X" && b[7].text == "X" && b[8].text == "X" || b[0].text == "X" && b[3].text == "X" && b[6].text == "X" || b[1].text == "X" && b[4].text == "X" && b[7].text == "X" || b[2].text == "X" && b[5].text == "X" && b[8].text == "X" || b[0].text == "X" && b[4].text == "X" && b[8].text == "X" || b[2].text == "X" && b[4].text == "X" && b[6].text == "X")
+        {
+            
+
+        }
+        //if()
+        /* else if (b[0].text == "X" && b[1].text == "X")
+         {
+             b[2].text = "0";
+         }
+         else if (b[3].text == "x" && b[4].text == "X")
+         { 
+
+         }*/
+    }
+    public void OnChange()
+    {
+        SceneManager.LoadScene("num");
+    }
+    void tru()
+    {
+        d[0].interactable = true;
+        d[1].interactable = true;
+        d[2].interactable = true;
+        d[3].interactable = true;
+        d[4].interactable = true;
+        d[5].interactable = true;
+        d[6].interactable = true;
+        d[7].interactable = true;
+        d[8].interactable = true;
+       
+    }
+    void clean()
+    {
+        for (int i = 0; i <=8; i++)
+        {
+            b[i].text = "";
+        }
+    }
+    void fal()
+    {
+        d[0].interactable = false;
+        d[1].interactable = false;
+        d[2].interactable = false;
+        d[3].interactable = false;
+        d[4].interactable = false;
+        d[5].interactable = false;
+        d[6].interactable = false;
+        d[7].interactable = false;
+        d[8].interactable = false;
+    }
+}
